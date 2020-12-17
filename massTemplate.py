c0b59207fa8824860ff42ca6b46e77d841af7176
amizukam <a.mizukami.0507@gmail.com>
2020-12-17 19:56:45 +0900

    Add massTemplate.py

---
+++ massTemplate.py
@@ -0,0 +1,9 @@
+import ROOT as r
+import sys
+sys.path.append("/Users/amizukam/DVJets/atlasstyle")
+import os, datetime
+from AtlasStyle import *
+from AtlasLabel import *
+
+r.gROOT.SetBatch()
+SetAtlasStyle()
