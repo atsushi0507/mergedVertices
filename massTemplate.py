c298fbc76bc96840caf44c39b94c2cf8b0e6d355
amizukam <atsushi.mizukami@cern.ch>
2020-12-18 09:58:00 +0900

    modified

--- massTemplate.py
+++ massTemplate.py
@@ -1,6 +1,7 @@
 import ROOT as r
 import sys
-sys.path.append("/Users/amizukam/DVJets/atlasstyle")
+#sys.path.append("/Users/amizukam/DVJets/atlasstyle")
+sys.path.append("/Users/atsushi/mvstudy_dv")
 import os, datetime
 from AtlasStyle import *
 from AtlasLabel import *
@@ -20,9 +21,9 @@ if (not os.path.isdir(directory)):
 
 c1 = r.TCanvas("c1", "c1", 800, 600)
 
-DV_m_4track = inputFile.Get("DV_m_4track")
-mergedMass4 = inputFile.Get("mergedMass4_mixed")
-mergedMass4_sigWeight = inputFile.Get("mergedMass4_sigWeight")
+DV_m_4track = inputFile.Get("DV_m_4track").Rebin(10)
+mergedMass4 = inputFile.Get("mergedMass4_mixed").Rebin(10)
+mergedMass4_sigWeight = inputFile.Get("mergedMass4_sigWeight").Rebin(10)
 
 bin1 = DV_m_4track.FindBin(100)
 DV_m_4track.GetXaxis().SetRange(1, bin1)
@@ -44,26 +45,44 @@ leg.SetTextSize(0.03)
 
 r.gPad.SetLogy()
 
-mergedMass4.Sumw2()
 mergedMass4.Scale(sf)
-mergedMass4_sigWeight.Sumw2()
 mergedMass4_sigWeight.Scale(sf)
-DV_m_4track.Draw()
+DV_m_4track.Draw("e")
 mergedMass4.Draw("same hist e0")
 mergedMass4_sigWeight.Draw("same hist e0")
-leg.AddEntry(DV_m_4track, "Data", "p")
+leg.AddEntry(DV_m_4track, "Data", "l")
 leg.AddEntry(mergedMass4, "100% merged", "l")
-leg.AddEntry(mergedMass4, "apply merge rate", "l")
+leg.AddEntry(mergedMass4_sigWeight, "apply merge rate", "l")
+leg.Draw()
+ATLASLabel(0.4, 0.88, label)
+c1.Print("{}/{}.pdf".format(directory, "mergedMass4_logy"))
+
+mergedMass4_sigWeight.Draw("hist e0")
+ATLASLabel(0.4, 0.88, label)
+c1.Print("{}/{}.pdf".format(directory, "mergedMass4_sigWeight_logy"))
+mergedMass4_sigWeight.Write()
+
+r.gPad.SetLogy(0)
+DV_m_4track.Draw("p")
+mergedMass4.Draw("same hist e0")
+mergedMass4_sigWeight.Draw("same hist e0")
 leg.Draw()
 ATLASLabel(0.4, 0.88, label)
 c1.Print("{}/{}.pdf".format(directory, "mergedMass4"))
 
-
 mergedMass4_sigWeight.Draw("hist e0")
 ATLASLabel(0.4, 0.88, label)
 c1.Print("{}/{}.pdf".format(directory, "mergedMass4_sigWeight"))
-mergedMass4_sigWeight.Write()
 
 bin10 = mergedMass4_sigWeight.FindBin(10)
 binMax = mergedMass4_sigWeight.GetNbinsX()
 print(mergedMass4_sigWeight.Integral(bin10, binMax))
+
+r.gPad.SetLogy()
+
+regions = ["inside_BP", "inside_IBL", "PIX", "inside_SCT"]
+DV_m_region = []
+mergedMass4_region = []
+mergedMass4_sigWeight_region = []
+for region in regions:
+    
